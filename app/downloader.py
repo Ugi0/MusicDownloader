1228364c0b760c99e81fe02dd2406c43232700d0
Ugi <90718205+Ugi0@users.noreply.github.com>
2025-06-16 22:25:53 +0300

    Check the final_path variable

--- app/downloader.py
+++ app/downloader.py
@@ -24,14 +24,15 @@ def download_file(id: str, title: str, author: str, format: str):
     #Download song
     try:
         with yt_dlp.YoutubeDL(yt_opts) as ydl:
-            ydl.download(id)
-            if final_path is None:
-                raise Exception("Download failed, no final path found.")
-            with taglib.File(final_path, save_on_exit=True) as song:
-                song.tags["ARTIST"] = author
-                song.tags["TITLE"] = title
-                song.tags["FORMAT"] = format
-            shutil.copy2(final_path, f'/app/storage/{id}')
+            ydl.download([id])
+        print(final_path)
+        if final_path is None:
+            raise Exception("Download failed, no final path found.")
+        with taglib.File(final_path, save_on_exit=True) as song:
+            song.tags["ARTIST"] = author
+            song.tags["TITLE"] = title
+            song.tags["FORMAT"] = format
+        shutil.copy2(final_path, f'/app/storage/{id}')
     except Exception as e:
         print(f"Error while downloading: {e}")
         return f"Error while downloading: {e}", 503
